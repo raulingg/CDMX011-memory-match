2196b18768cb66d8534fc7ceffa355b4c755af4e
Raul Robinson Quispe Mendez <9435850+raulingg@users.noreply.github.com>
2021-09-12 12:34:16 -0500

    wip

---
+++ src/data/lol/lol.js
@@ -0,0 +1,59 @@
+export default {
+  name: "Pokemon",
+  items: [
+    {
+      id: "Aatrox",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+      bgColor: "#339933"
+    },
+    {
+      id: "Ahri",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg",
+      bgColor: "#339933"
+    },
+    {
+      id: "Akali",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg",
+      bgColor: "#339933"
+    },
+    {
+      id: "Alistar",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Alistar_0.jpg",
+      bgColor: "#F05032"
+    },
+    {
+      id: "Amumu",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Amumu_0.jpg",
+      bgColor: "#F05032"
+    },
+    {
+      id: "Anivia",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Anivia_0.jpg",
+      bgColor: "#F05032"
+    },
+    {
+      id: "Annie",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Annie_0.jpg",
+      bgColor: "#1572B6"
+    },
+    {
+      id: "Ashe",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ashe_0.jpg",
+      bgColor: "#1572B6"
+    },
+    {
+      id: "AurelionSol",
+      image:
+        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/AurelionSol_0.jpg",
+      bgColor: "#1572B6"
+    }
+  ]
+};
--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
-import App from './components/App.js';
+import App from "./components/App.js";
+import data from "./data/lol/lol.js";
 
-document.getElementById('root').appendChild(App());
+console.log("hola");
+document.getElementById("app").appendChild(App());
+
+const render = (champions) => {
+  let html = "";
+  champions.forEach(function (champion) {
+    html += `
+      <div class="card">
+        <div class="image--circular">
+          <img src="${champion.splash}">
+        </div>
+        <h5>${champion.name}</h5>
+      </div>
+    `;
+  });
+
+  //cardsContainer.innerHTML = html;
+};
+
+//render(champions);
