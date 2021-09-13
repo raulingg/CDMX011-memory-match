0ae54b1c3f3289489adf3295001de04baf89513c
Raul Quispe <relaxedchild@gmail.com>
2021-09-12 22:52:39 -0500

    wip

--- src/data/lol/lol.js
+++ src/data/lol/lol.js
@@ -1,58 +1,56 @@
 export default {
-  name: "Pokemon",
+  name: "League Of Legends",
   items: [
     {
       id: "Aatrox",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt570145160dd39dca/5db05fa8347d1c6baa57be25/RiotX_ChampionList_aatrox.jpg?quality=90&width=250",
       bgColor: "#339933"
     },
     {
       id: "Ahri",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt1259276b6d1efa78/5db05fa86e8b0c6d038c5ca2/RiotX_ChampionList_ahri.jpg?quality=90&width=250",
       bgColor: "#339933"
     },
     {
       id: "Akali",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt80ff58fe809777ff/5db05fa8adc8656c7d24e7d6/RiotX_ChampionList_akali.jpg?quality=90&width=250",
       bgColor: "#339933"
     },
     {
       id: "Alistar",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Alistar_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt3b366925d2fd8fdb/5db05fa856458c6b3fc1750b/RiotX_ChampionList_alistar.jpg?quality=90&width=250",
       bgColor: "#F05032"
     },
     {
       id: "Amumu",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Amumu_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt40dfbe48a61c439f/5db05fa80b39e86c2f83dbf9/RiotX_ChampionList_amumu.jpg?quality=90&width=250",
       bgColor: "#F05032"
     },
     {
       id: "Anivia",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Anivia_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt3d24a1482453088a/5db05fa8df78486c826dcce8/RiotX_ChampionList_anivia.jpg?quality=90&width=250",
       bgColor: "#F05032"
     },
     {
       id: "Annie",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Annie_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt28c708665427aef6/5db05fa89481396d6bdd01a6/RiotX_ChampionList_annie.jpg?quality=90&width=250",
       bgColor: "#1572B6"
     },
     {
       id: "Ashe",
-      image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ashe_0.jpg",
-      bgColor: "#1572B6"
+      image: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt943aae038e2dee98/5db05fa8e9effa6ba5295f91/RiotX_ChampionList_ashe.jpg?quality=90&width=250"
     },
     {
       id: "AurelionSol",
       image:
-        "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/AurelionSol_0.jpg",
+        "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt5dd3569fc67d6664/5db05fa8bd24496c390ae4d8/RiotX_ChampionList_aurelionsol.jpg?quality=90&width=250",
       bgColor: "#1572B6"
     }
   ]
--- src/main.js
+++ src/main.js
@@ -1,23 +1,8 @@
 import App from "./components/App.js";
 import data from "./data/lol/lol.js";
 
-console.log("hola");
-document.getElementById("app").appendChild(App());
+document.getElementById("app").appendChild(App({
+  title: data.name,
+  cards: data.items
+}));
 
-const render = (champions) => {
-  let html = "";
-  champions.forEach(function (champion) {
-    html += `
-      <div class="card">
-        <div class="image--circular">
-          <img src="${champion.splash}">
-        </div>
-        <h5>${champion.name}</h5>
-      </div>
-    `;
-  });
-
-  //cardsContainer.innerHTML = html;
-};
-
-//render(champions);
